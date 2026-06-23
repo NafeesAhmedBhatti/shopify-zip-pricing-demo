12e5b202f837ebc42a1fca101e14f8b44832e024
NafeesAhmedBhatti <nafees6005@gmail.com>
2026-06-23 19:52:08 +0530

    Initial commit - Shopify ZIP pricing demo

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/get-price", (req, res) => {
+  const { zipCode } = req.body;
+
+  const prices = {
+    "75028": 1499,
+    "10001": 1699,
+    "90210": 1799
+  };
+
+  if (prices[zipCode]) {
+    return res.json({
+      success: true,
+      price: prices[zipCode]
+    });
+  }
+
+  return res.json({
+    success: false,
+    message: "No pricing available for this ZIP code"
+  });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
